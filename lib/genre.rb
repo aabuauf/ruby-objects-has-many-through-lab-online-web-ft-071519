0a87c0793cd888f5f4d2e7d76281fb88dc9be3d4
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-28 12:44:07 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,7 +1,7 @@
 class Genre
   @@all = [] 
-  attr_reader :name, :genre
-  def initialize(name,artist,genre)
+  attr_reader :name
+  def initialize(name)
     @name = name
     @artist = artist
     @genre = genre
