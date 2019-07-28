2d271277b447a08f5ede9168baf78daac15c6bff
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-28 12:47:57 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,12 +3,23 @@ class Genre
   attr_reader :name
   def initialize(name)
     @name = name
-    @artist = artist
-    @genre = genre
-    @@all << song
+    @@all << self
   end
   
   def self.all
     @@all
+  end
+  
+  def songs
+    Song.all.select do|eachSong|
+        self ==eachSong.genre
+    end
+  end
+  
+  def artists
+    genreSongs = songs
+    genreSongs.all do|eachSong|
+    
+    end
   end
 end
