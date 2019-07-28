00f7ab05192f80104c2ae9012e0e2cb7ced496d4
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-28 12:35:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song
+  @@all = [] 
   attr_reader :name, :genre
   def initialize(name,artist,genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@all << song
+  end
+  
+  def self.all
+    @@all
   end
 end
