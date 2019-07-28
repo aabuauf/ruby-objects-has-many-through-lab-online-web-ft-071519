940f3a90e0310941291ba8da3f95588c8ae3279c
Ahmed Abououf <aabuauf@yahoo.com>
2019-07-28 13:16:01 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,7 +11,7 @@ class Patient
   end
   
   def new_appointment
-    appointment = Appointment.new(self,doctor, date)
+    appointment = Appointment.new(self, doctor, date)
   end
   
   def appointments
